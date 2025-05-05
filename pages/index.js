dae67a34b5302dc11544653b1ab6f49d971ef16a
ahmdhk9 <ahmdhk9@users.noreply.github.com>
2025-05-05 06:54:08 +0300

    Update and rename index.html to pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>منصة تداول ذكية</title>
+      </Head>
+      <main>
+        <h1>أهلاً بك في منصة التداول الذكية</h1>
+      </main>
+    </div>
+  );
+}
